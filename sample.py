318ed517bb9ebc6f310b11faeccaee1479ffc4e1
Ymmiblubbs <ymir21@ru.is>
2022-03-04 12:24:43 +0000

    Bjó til test sem athugar hvort talan sé deilanleg með 3. Ef svo er á outputið að vera Buzz. Svo gerði ég fall í sample.py sem hefur einmitt þessa virkni

--- sample.py
+++ sample.py
@@ -1,3 +1,5 @@
 def Fizzbuzz(int):
     if int % 5 == 0 and int % 3 == 0:
         return "Fizzbuzz"
+    if int % 5 == 0:
+        return "Fizz"
--- test_sample.py
+++ test_sample.py
@@ -4,6 +4,7 @@ def test_divisible_by_5_and_3():
     assert FizzBuzz(15) == "FizzBuzz"
     assert FizzBuzz(30) == "FizzBuzz"
 
-
-
+def test_divisble_by_5():
+    assert FizzBuzz(5) == "Buzz"
+    assert FizzBuzz(10) == "Buzz"
     
